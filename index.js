d491fffdd1f9053085e342c1114ee53d34b9d895
Yohanes William <william.gunawan@live.com>
2017-11-08 10:13:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,13 +18,20 @@ function divide(a, b) {
   return a / b
 }
 
-function inc(a) {
-  a++
-  return a
+function inc(n) {
+  n++
+  return n
 }
 
-function dec(a) {
-  a--
-  return a
+function dec(n) {
+  n--
+  return n
 }
 
+function makeInt(n) {
+  return parseInt('n', 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat('n')
+}
