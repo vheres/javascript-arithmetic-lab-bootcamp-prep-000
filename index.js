21b0b102f409aa4a5baa2b08f4e69bce89cf871b
Yohanes William <william.gunawan@live.com>
2017-11-08 10:09:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ b = Math.floor(Math.random() * 1000)
 function add(a, b) {
   return a + b
 }
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
